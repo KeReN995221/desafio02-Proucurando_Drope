8757bf660748937a7e0df3926924d126f6b752b6
Keren Apuque <cardosokerenapuque@gmail.com>
2024-05-06 21:00:35 -0300

    projeto criado

---
+++ alunos_matriculas/src/main/java/com/desafio02/alunos_matriculas/AlunosMatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.desafio02.alunos_matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunosMatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunosMatriculasApplication.class, args);
+	}
+
+}
