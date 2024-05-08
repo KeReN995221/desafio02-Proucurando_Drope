74871431db2642908e381306211c52d66256d7a7
Vinicius Fillos <vinifillos@gmail.com>
2024-05-08 20:44:22 -0300

    feat: : tratamento de exeptions,ExceptionHandler e Error message implementados

--- microServiceCursos/src/main/java/com/desafio02/cursos/services/CursoService.java
+++ microServiceCursos/src/main/java/com/desafio02/cursos/services/CursoService.java
@@ -1,11 +1,14 @@
 package com.desafio02.cursos.services;
 
 import com.desafio02.cursos.entities.Curso;
+import com.desafio02.cursos.excpetions.EntityNotFoundException;
+import com.desafio02.cursos.excpetions.NameUniqueViolationException;
+import com.desafio02.cursos.excpetions.UnableCourseException;
 import com.desafio02.cursos.repositories.CursoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 @RequiredArgsConstructor
@@ -13,26 +16,32 @@ public class CursoService {
 
     private final CursoRepository cursoRepository;
 
-    @PostMapping
     @Transactional
     public Curso salvar(Curso curso) {
-        return cursoRepository.save(curso);
+        try {
+            return cursoRepository.save(curso);
+        } catch (DataIntegrityViolationException ex) {
+            throw new NameUniqueViolationException("O nome do curso deve ser único");
+        }
     }
 
+    @Transactional
     public Curso mudarProfessor(Long id, Curso curso) {
         Curso novoCurso = cursoRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Curso não encontrado")
+                () -> new EntityNotFoundException("Curso não encontrado")
         );
         novoCurso.setProfessor(curso.getProfessor());
         cursoRepository.save(novoCurso);
         return novoCurso;
     }
 
+    @Transactional
     public Curso desabilitarCurso(Long id) {
         Curso curso = cursoRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Curso não encontrado")
+                () -> new EntityNotFoundException("Curso não encontrado")
         );
-        curso.setAtivo(false);
+        if (curso.isAtivo()) curso.setAtivo(false);
+        else throw new UnableCourseException("O curso já esta desabilitado");
         cursoRepository.save(curso);
         return curso;
     }
