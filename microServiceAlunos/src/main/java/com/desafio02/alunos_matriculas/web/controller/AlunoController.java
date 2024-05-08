02fda6566be051b2f97c8ca5630b08727f7e65c6
Keren Apuque <cardosokerenapuque@gmail.com>
2024-05-08 09:34:48 -0300

    feat:  adicionada a função de criar alunos

--- microServiceAlunos/src/main/java/com/desafio02/alunos_matriculas/web/controller/AlunoController.java
+++ microServiceAlunos/src/main/java/com/desafio02/alunos_matriculas/web/controller/AlunoController.java
@@ -1,13 +1,26 @@
 package com.desafio02.alunos_matriculas.web.controller;
 
+import com.desafio02.alunos_matriculas.entities.Aluno;
 import com.desafio02.alunos_matriculas.services.AlunoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/alunos")
 public class AlunoController {
     private final AlunoService alunoService;
+
+    @PostMapping
+    public ResponseEntity<Aluno> cadastrarAluno(@RequestBody Aluno aluno) {
+        alunoService.salvar(aluno);
+        return ResponseEntity.ok(aluno);
+    }
 }
