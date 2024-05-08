955b3a6b98591f2da360cf5747ad90cc82ea29d5
Vinicius Fillos <vinifillos@gmail.com>
2024-05-08 11:22:10 -0300

    fix: consertado o bug de fechar função

--- microServiceCursos/src/main/java/com/desafio02/cursos/services/CursoService.java
+++ microServiceCursos/src/main/java/com/desafio02/cursos/services/CursoService.java
@@ -23,7 +23,8 @@ public class CursoService {
         Curso novoCurso = cursoRepository.findByNome(nome);
         novoCurso.setProfessor(curso.getProfessor());
         return novoCurso;
-  
+    }
+
     public Curso desabilitarCurso(Long id) {
         Curso curso = cursoRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Curso não encontrado")
