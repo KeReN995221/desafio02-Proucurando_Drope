e87e50a16df2c57e4d53859451a5a2f5d3a5fa0b
Vinicius Fillos <vinifillos@gmail.com>
2024-05-08 09:25:44 -0300

    feat: adicionada a função de criar cursos

--- microServiceCursos/src/main/java/com/desafio02/cursos/web/controller/CursoController.java
+++ microServiceCursos/src/main/java/com/desafio02/cursos/web/controller/CursoController.java
@@ -1,7 +1,11 @@
 package com.desafio02.cursos.web.controller;
 
+import com.desafio02.cursos.entities.Curso;
 import com.desafio02.cursos.services.CursoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CursoController {
 
     private final CursoService cursoService;
+
+    @PostMapping
+    public ResponseEntity<Curso> create(@RequestBody Curso curso) {
+        Curso newCurso = cursoService.salvar(curso);
+        return ResponseEntity.status(201).body(newCurso);
+    }
 }
