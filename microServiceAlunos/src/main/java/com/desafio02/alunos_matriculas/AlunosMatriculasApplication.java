682b0bce4a22c957b324850982c61ec75aeb2785
Keren Apuque <cardosokerenapuque@gmail.com>
2024-05-08 21:24:56 -0300

    feat: implementado os métodos getById e getAll

--- microServiceAlunos/src/main/java/com/desafio02/alunos_matriculas/AlunosMatriculasApplication.java
+++ microServiceAlunos/src/main/java/com/desafio02/alunos_matriculas/AlunosMatriculasApplication.java
@@ -2,6 +2,7 @@ package com.desafio02.alunos_matriculas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class AlunosMatriculasApplication {
