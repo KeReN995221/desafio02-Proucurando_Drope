debda229f32c562042da9c35d279d777ea9ad5eb
nicoleinae <nicoleinae@gmail.com>
2024-05-09 21:59:06 -0300

    feat: implementação da funcionalidade de matricular alunos a determinados cursose as dependencias necessárias

--- microServiceAlunos/src/main/java/com/desafio02/alunos_matriculas/AlunosMatriculasApplication.java
+++ microServiceAlunos/src/main/java/com/desafio02/alunos_matriculas/AlunosMatriculasApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AlunosMatriculasApplication {
 
 	public static void main(String[] args) {
