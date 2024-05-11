12eaddcc1bb040595547cf96a1abccfb2cf6ddf1
Keren Apuque <cardosokerenapuque@gmail.com>
2024-05-10 22:14:35 -0300

    fix: alterado o construtor curso no teste

--- microServiceCursos/src/test/java/com/desafio02/cursos/resources/CursoConstantes.java
+++ microServiceCursos/src/test/java/com/desafio02/cursos/resources/CursoConstantes.java
@@ -5,6 +5,6 @@ import com.desafio02.cursos.entities.Curso;
 import static com.desafio02.cursos.entities.Curso.AreaDoConhecimento.MEDICINA;
 
 public class CursoConstantes {
-    public static final Curso CURSO = new Curso(3L,"Odontologia", 350, "Ayslan Zotesso", MEDICINA, true);
-    public static final Curso CURSO_INVALIDO = new Curso(0L,"", 0, "", null, false);
+    public static final Curso CURSO = new Curso(3L,"Odontologia", 350, "Ayslan Zotesso", MEDICINA, true, 3);
+
 }
